06aeadb3b231cead0999c2c78654b761e8716ac0
Mickie Zhou <mickiedd@gmail.com>
2019-06-01 21:31:26 +0800

    add a PositionReport c++ component to the chair

---
+++ Source/UdemyProject1/UdemyProject1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UdemyProject1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UdemyProject1, "UdemyProject1" );
