85361fb232efc618935d0c0aaf8c60f141387401
Mickie Zhou <mickiedd@gmail.com>
2019-06-02 12:27:30 +0800

    add object controller to edit the rotation.

--- Source/UdemyProject1/OpenDoor.cpp
+++ Source/UdemyProject1/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PositionReport.h"
-#include "Gameframework/Actor.h"
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,21 +16,26 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
 
-	FString ObjectName =  GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Error, TEXT("Oh what is this for %s is at %s"), *ObjectName, *ObjectPos);
+	// Find the owning Actor
+	AActor* Owner = GetOwner();
+
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.f, 45.f, 0.f);
+
+	// Set the door rotation
+	Owner->SetActorRotation(NewRotation);
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
